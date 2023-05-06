adc55d51473639fc0d28a3d6b22a316d0aa38b25
Bananahexagon <100505456+Bananahexagon@users.noreply.github.com>
2023-05-06 10:33:53 +0900

    a

--- src/font.js
+++ src/font.js
@@ -7,10 +7,21 @@ async function font(input) {
 function writeFrame(input) {
     const chars = input.string;
     const size = input.size;
+    const d = input.direction * Math.PI / 180;
     let x, y;
     [x, y] = [0, 0];
     for (let i = 0; i < chars.length; i++) {
         let charData = Global.fontData[chars[i]];
-        Core.stamp("determination_white", x * size / 100, (y + charData.gap / 2) * size / 100, 0, size, 1, charData.left, charData.up, charData.width, charData.height)
-    }
+        if (chars[i] == "\n") {
+            x = 0;
+            y++;
+        } else {
+            Core.stamp("determination_white",
+                input.x + (Math.cos(d) * x + Math.sin(d) * y) * size / 100,
+                input.y + (Math.sin(d) * x - Math.cos(d) * y + charData.gap / 2) * size / 100,
+                0, size, 1, charData.left, charData.up, charData.width, charData.height
+            )
+            x += charData.width + 2;
+        }
+    };
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 "use strict"
 
 function main() {
-    Game.timer = 0
+    Game.timer = 0;
     window.requestAnimationFrame(update);
 }
 function update() {
-    if (Core.inputKeys.up) Game.player.y -= 5
-    if (Core.inputKeys.down) Game.player.y += 5
-    if (Core.inputKeys.left) Game.player.x -= 5
-    if (Core.inputKeys.right) Game.player.x += 5
+    if (Core.inputKeys.up) Game.player.y -= 5;
+    if (Core.inputKeys.down) Game.player.y += 5;
+    if (Core.inputKeys.left) Game.player.x -= 5;
+    if (Core.inputKeys.right) Game.player.x += 5;
 }
